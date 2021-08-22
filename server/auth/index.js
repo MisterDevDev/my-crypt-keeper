436589fbf2cdf6eadfecbca3594919241622ba36
djb91c <djb91c@gmail.com>
2021-08-22 15:37:08 -0400

    heroku fix env

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
-const {coinbase_public} = process.env.coinbase_public || require('./env')
-const {coinbase_secret} = process.env.coinbase_secret || require('./env')
+const coinbase_public = process.env.coinbase_public || (require('./env')).coinbase_public
+const coinbase_secret = process.env.coinbase_secret || require('./env').coinbase_secret
 module.exports = router
 
 router.get('/oauth', async (req, res, next) => {
