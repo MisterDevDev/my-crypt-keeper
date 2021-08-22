46baa312588b2a76c546739719c985232509ed02
djb91c <djb91c@gmail.com>
2021-08-21 21:44:22 -0400

    defined config var in db

--- server/db/db.js
+++ server/db/db.js
@@ -3,6 +3,10 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name
 
+const config = {
+  logging: false
+};
+
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
 if(process.env.DATABASE_URL){
     config.dialectOptions = {
