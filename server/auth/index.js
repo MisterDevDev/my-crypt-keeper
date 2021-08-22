b39183ed07a8150b33f80c97b585aa7a128ece76
djb91c <djb91c@gmail.com>
2021-08-22 15:32:17 -0400

    fixing heroku

--- server/auth/index.js
+++ server/auth/index.js
@@ -4,6 +4,8 @@ const {coinbase_secret} = process.env.coinbase_secret || require('./env')
 module.exports = router
 
 router.get('/oauth', async (req, res, next) => {
+    console.log('heroku env variable= ~~~~~~',process.env.coinbase_public)
+    console.log('public variable i pass= ~~~~~~', coinbase_public)
     try {
         res.send(coinbase_public)
     } catch (error) {
